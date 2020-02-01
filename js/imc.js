5d38e8081790b1140b7a2033ae6311f149433ff2
fernandomarca <59258828+fernandomarca@users.noreply.github.com>
2020-02-01 00:07:16 -0300

    Update imc.js

--- js/imc.js
+++ js/imc.js
@@ -1,54 +1,36 @@
-
-/*alert("olá mundo");
-função ligar
-autor:Fernando
-descrição: apresenta o nome do usuário e liga a lampada.
-*/
-/*
-	function ligar(){
-		var nome="ricardo\n fernando";//string
-		var idade =31;//int
-		var casado = true;//boolean
-		document.getElementById("tokyo").href="http://www.google.com.br";
-		alert(nome);
-		}
-*/
-/*botão de interacao*/
-/*
-function ligar(){
-		var nome= prompt("qual o seu nome?");//string
-		alert("Prazer em conhecer você " + nome);
-		document.getElementById("tokyo");
-		
-		}
-		*/
-/* variablename = (condition) ? value1:value2	
-voteable = (age < 18) ? "Too young":"Old enough";	
-*/		
-/* 
-Pessoa = {
-	nome:"fulano"
-}		
-alert(Pessoa.nome);
-delete Pessoa.nome;
-alert(Pessoa.nome);
-*/
-/*
-Pessoa = {
-	nome:"fulano"
-}		
-alert("nome" in Pessoa);
-alert("telefone" in Pessoa);
-*/
 function calcularIMC(){
 	var formulario = document.getElementById("formulario");
+	
 	var kilos = +formulario.kilos.value;
 	var metros = +formulario.metros.value;
-	var centimetros = +formulario.centimetros.value;
-	var altura = (metros *100 + centimetros)/100;
-	var imc = kilos / (altura * altura);
+	var imc = kilos / (metros * metros);
 	formulario.imc.value=imc.toFixed(2);
+	
+		if(kilos <=0){
+			alert("entre com um valor positivo para kilos");
+		}
+	
+		if(metros <=0){
+			alert("entre com um valor positivo para altura");
+			formulario.imc.value="erro!";
+		}
+	
+	if(imc <=0){
+		formulario.resultado.value = "erro!";
+	}else if(imc<20){
+		formulario.resultado.value = "abaixo do peso";
+	}else if (imc>20 && imc <=25){
+		formulario.resultado.value = "peso ideal";
+	}else if (imc>25 && imc<=30){
+		formulario.resultado.value = "sobrepeso";
+	}else if (imc>30 && imc<=35){
+		formulario.resultado.value = "obesidade moderada";
+	}else if (imc>35 && imc<=40){
+		formulario.resultado.value = "obesidade Mórbida";
+	}else if(imc>40 && imc<=50){
+		formulario.resultado.value = "super obesidade";
+	}else
+	{
+		alert("dados incorretos");
+	}
 }
-
-
-
