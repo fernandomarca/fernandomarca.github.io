186a9a18a01eaf27377fe0138c9ddaf57bed71f9
fernandomarca <fernandomarca@hotmail.com>
2020-09-13 22:23:06 -0300

    header

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Header from './components/Header/Header';
+
+//http://preview.themeforest.net/item/decent-material-cv-resume/full_screen_preview/17454569?_ga=2.228767836.1955276994.1600044995-1624887230.1588608096
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Em contrução...</h2>
-      </header>
+    <div>
+      <Header></Header>
     </div>
   );
 }
