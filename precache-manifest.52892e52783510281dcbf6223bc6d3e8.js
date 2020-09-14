303ac2dc9b003a9ce3edb0b301c91391caa19abd
fernandomarca <fernandomarca@hotmail.com>
2020-09-13 21:30:22 -0300

    Updates

---
+++ precache-manifest.52892e52783510281dcbf6223bc6d3e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eb58d6915303d4c3e50a6f95de0f0b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80f005fdb0906384a21b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ec15ef7eef509181eba4",
+    "url": "/static/js/2.799510d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.799510d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80f005fdb0906384a21b",
+    "url": "/static/js/main.acbf6346.chunk.js"
+  },
+  {
+    "revision": "16d50208a04408d1bf24",
+    "url": "/static/js/runtime-main.23041e79.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
