c393e7d9b61ce08ec4f1e973964f4693b7f35fb3
fernandomarca <fernandomarca@hotmail.com>
2020-11-01 17:24:30 -0300

    header

--- site/src/components/Header/Header.tsx
+++ site/src/components/Header/Header.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import './Header.css'
+
 const Header = () => {
     return (
-        <div>
-            header
+        <div className="header-container">
+            <nav className="header-nav">
+                <ul>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Experiência</a></li>
+                    <li><a href="">Skills</a></li>
+                    <li><a href="">Formação</a></li>
+                    <li><a href="">Contato</a></li>
+                    <li><a href="">Serviços-PJ</a></li>
+                </ul>
+            </nav>
         </div>
     )
 }
